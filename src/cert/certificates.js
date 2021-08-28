eedab215ad78a63e3b7bbc369d07ca0755822821
Ahmed Zeno <ahmad.zeino@gmail.com>
2021-08-28 13:23:58 +0200

    style: add dummy text

--- src/cert/certificates.js
+++ src/cert/certificates.js
@@ -1,6 +1,6 @@
 export const certificatesList = [
     {id:0,name:"cert_1",src:`https://robohash.org/${Math.random()*100}?200x200`,desc:"this certificate is about ..."},
-    {id:1,name:"aws_2",src:`https://robohash.org/${Math.random()*100}?200x200`,desc:"this certificate is about ..."},
+    {id:1,name:"aws_2",src:`https://robohash.org/${Math.random()*100}?200x200`,desc:"this certificate is about some other ..."},
     {id:2,name:"cert_3",src:`https://robohash.org/${Math.random()*100}?200x200`,desc:"this certificate is about ..."},
     {id:3,name:"cert_4",src:`https://robohash.org/${Math.random()*100}?200x200`,desc:"this certificate is about ..."},
     {id:4,name:"cert_5",src:`https://robohash.org/${Math.random()*100}?200x200`,desc:"this certificate is about ..."},
